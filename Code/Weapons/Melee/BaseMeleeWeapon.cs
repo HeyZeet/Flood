128cb7e05ca270c66929dbd74b4904fbc131563b
HeyZeet <139825201+HeyZeet@users.noreply.github.com>
2026-05-16 13:08:30 -0500

    Refactor melee attack method and add helper functions
    Refactored melee attack logic to improve readability and maintainability. Added TryDamageHitObject and LogBuildPieceHit methods for better separation of concerns.

--- Code/Weapons/Melee/BaseMeleeWeapon.cs
+++ Code/Weapons/Melee/BaseMeleeWeapon.cs
@@ -14,53 +14,37 @@ public abstract class BaseMeleeWeapon : BaseWeapon
 	}
 
 	protected virtual void DoMeleeAttack()
-    {
-	    var owner = OwnerPlayer;
+	{
+		var owner = OwnerPlayer;
 
-	    if ( !owner.IsValid() )
-	    {
-		    Log.Warning( $"{DisplayName} has no valid owner player." );
-		    return;
-	    }
+		if ( !owner.IsValid() )
+		{
+			Log.Warning( $"{DisplayName} has no valid owner player." );
+			return;
+		}
 
-	    var start = GetAttackStart();
-	    var end = start + GetAttackDirection() * Range;
+		var start = GetAttackStart();
+		var end = start + GetAttackDirection() * Range;
 
-	    var tr = Scene.Trace
-		    .Sphere( TraceRadius, start, end )
-		    .IgnoreGameObjectHierarchy( owner.GameObject )
-		    .Run();
+		var tr = Scene.Trace
+			.Sphere( TraceRadius, start, end )
+			.IgnoreGameObjectHierarchy( owner.GameObject )
+			.Run();
 
-	    if ( DrawDebugTrace )
-	    {
-		    DebugOverlay.Trace( tr, 1f );
-	    }
+		if ( DrawDebugTrace )
+			DebugOverlay.Trace( tr, 1f );
 
-	    if ( !tr.Hit )
-	    {
-		    Log.Info( $"{DisplayName} missed." );
-		    return;
-	    }
+		if ( !tr.Hit )
+		{
+			Log.Info( $"{DisplayName} missed." );
+			return;
+		}
 
-	    Log.Info( $"{DisplayName} hit {tr.GameObject.Name}." );
+		Log.Info( $"{DisplayName} hit {tr.GameObject.Name}." );
 
-        var buildPiece = tr.GameObject.Components.Get<BuildPiece>( FindMode.EverythingInSelfAndAncestors );
-
-        if ( buildPiece.IsValid() )
-        {
-	        Log.Info( $"Hit build piece: {buildPiece.DisplayName}, Material: {buildPiece.Material}, Cost: {buildPiece.Cost}" );
-        }
-
-        var damageable = tr.GameObject.Components.Get<DamageableComponent>( FindMode.EverythingInSelfAndAncestors );
-
-        if ( damageable.IsValid() )
-        {
-	        var damageInfo = DamageInfo.FromWeapon( this, tr );
-	        damageable.TakeDamage( damageInfo );
-
-	        Log.Info( $"{DisplayName} damaged {tr.GameObject.Name} for {Damage}." );
-        }
-    }
+		LogBuildPieceHit( tr );
+		TryDamageHitObject( tr );
+	}
 
 	protected virtual Vector3 GetAttackStart()
 	{
@@ -72,11 +56,10 @@ public abstract class BaseMeleeWeapon : BaseWeapon
 		var sceneCamera = Scene.Camera;
 
 		if ( sceneCamera.IsValid() )
-		return sceneCamera.WorldPosition;
+			return sceneCamera.WorldPosition;
 
 		return WorldPosition;
 	}
-
 
 	protected virtual Vector3 GetAttackDirection()
 	{
@@ -91,5 +74,29 @@ public abstract class BaseMeleeWeapon : BaseWeapon
 			return sceneCamera.WorldRotation.Forward;
 
 		return WorldRotation.Forward;
+	}
+
+	protected virtual void TryDamageHitObject( SceneTraceResult trace )
+	{
+		var damageable = trace.GameObject.Components.Get<DamageableComponent>( FindMode.EverythingInSelfAndAncestors );
+
+		if ( !damageable.IsValid() )
+			return;
+
+		var damageInfo = DamageInfo.FromWeapon( this, trace );
+
+		damageable.TakeDamage( damageInfo );
+
+		Log.Info( $"{DisplayName} damaged {trace.GameObject.Name} for {Damage}." );
+	}
+
+	private void LogBuildPieceHit( SceneTraceResult trace )
+	{
+		var buildPiece = trace.GameObject.Components.Get<BuildPiece>( FindMode.EverythingInSelfAndAncestors );
+
+		if ( !buildPiece.IsValid() )
+			return;
+
+		Log.Info( $"Hit build piece: {buildPiece.DisplayName}, Material: {buildPiece.Material}, Cost: {buildPiece.Cost}" );
 	}
 }
