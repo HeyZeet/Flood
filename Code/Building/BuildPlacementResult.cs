d5b9e4f6c02287492b4b6284dd7937115533cb06
HeyZeet <139825201+HeyZeet@users.noreply.github.com>
2026-05-16 12:47:10 -0500

    Remove unnecessary line breaks in BuildPlacementResult

--- Code/Building/BuildPlacementResult.cs
+++ Code/Building/BuildPlacementResult.cs
@@ -3,11 +3,8 @@ using Sandbox;
 public struct BuildPlacementResult
 {
 	public bool IsValid { get; set; }
-
 	public Vector3 Position { get; set; }
-
 	public Rotation Rotation { get; set; }
-
 	public string Reason { get; set; }
 
 	public static BuildPlacementResult Valid( Vector3 position, Rotation rotation )
